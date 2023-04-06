a9bbbccc74e40efd282202196a18b56010a7d76e
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-07 04:06:34 +0900

    Feat: [client] global.css, theme, fonts 초기 세팅 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,63 @@
+import '@styles/globals.css';
+
 import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+
+const pretendard = localFont({
+  src: [
+    { path: '../../public/font/Pretendard-Thin.woff2', weight: '100' },
+    { path: '../../public/font/Pretendard-ExtraLight.woff2', weight: '200' },
+    { path: '../../public/font/Pretendard-Light.woff2', weight: '300' },
+    { path: '../../public/font/Pretendard-Regular.woff2', weight: '400' },
+    { path: '../../public/font/Pretendard-Medium.woff2', weight: '500' },
+    { path: '../../public/font/Pretendard-SemiBold.woff2', weight: '600' },
+    { path: '../../public/font/Pretendard-Bold.woff2', weight: '700' },
+    { path: '../../public/font/Pretendard-ExtraBold.woff2', weight: '800' },
+    { path: '../../public/font/Pretendard-Black.woff2', weight: '900' },
+  ],
+  display: 'swap',
+  fallback: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Malgun Gothic',
+    'sans-serif',
+  ],
+});
+
+const GlobalStyle = createGlobalStyle`
+  * {
+  font-family: ${pretendard.style.fontFamily};
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <GlobalStyle />
+        {process.env.NODE_ENV !== 'production' ? (
+          <ReactQueryDevtools initialIsOpen={false} />
+        ) : null}
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Hydrate>
     </QueryClientProvider>
   );
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,32 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  // 가변 값
+  primary: '#FF6D3A',
+  secondary: '#4181F0',
+  background: '#F8F9FA',
+
+  // 고정 값
+  gray50: '#F8F9FA',
+  gray100: '#F1F3F5',
+  gray200: '#E9ECEF',
+  gray300: '#DEE2E6',
+  gray400: '#CCD2D8',
+  gray500: '#AEB5BA',
+  gray600: '#888E94',
+  gray650: '#868E96',
+  gray700: '#484F54',
+  gray750: '#3A3E43',
+  gray800: '#32363A',
+  gray850: '#26282B',
+  gray900: '#1F2022',
+  gray950: '#17181A',
+  white: '#ffffff',
+  black: '#000000',
+  negative: '#D82C0D',
+  negativeLight: '#FFF4F4',
+  positive: '#2C6ECB',
+  positiveLight: '#F2F7FE',
+};
+
+export default theme;
