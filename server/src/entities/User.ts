bdd23c9a5036e92b0b3b6f60a99f0caec251ce34
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-06 22:29:10 +0900

    Feat: [server] User Entity 정의

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,16 +1,57 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsEmail, Length } from 'class-validator';
+import { Exclude } from 'class-transformer';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  OneToMany,
+  BeforeInsert,
+} from 'typeorm';
+import BaseEntity from './Entity';
+import bcrypt from 'bcryptjs';
+// import Post from './Post';
+// import Vote from './Vote';
 
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+// 명시적으로 매핑할 테이블을 지정
+// 'users'라는 이름을 사용하여 User 엔티티가 users 테이블과 매핑되도록 설정
+@Entity('users')
+export default class User extends BaseEntity {
+  // 인덱스는 데이터베이스에서 데이터를 검색할 때 성능을 향상시키는 데 사용
+  // @Index()가 두 개인 이유는, email 필드와 username 필드 모두에 인덱스를 생성하도록 설정하기 위함, 빠르게 검색 가능
+  // email과 username은 각각 고유한 값이어야 하므로, 이를 강제하기 위해 unique 옵션도 함께 사용
+  @Index()
+  @IsEmail(undefined, { message: '이메일 주소가 잘못되었습니다.' })
+  @Length(1, 255, { message: '이메일 주소는 비워둘 수 없습니다.' })
+  @Column({ unique: true })
+  email: string;
 
+  @Index()
+  @Length(3, 32, { message: '사용자 이름은 3자 이상이어야 합니다.' })
+  @Column({ unique: true })
+  username: string;
+
+  @Exclude()
   @Column()
-  firstName: string;
+  @Length(6, 255, { message: '비밀번호는 6자리 이상이어야 합니다.' })
+  password: string;
 
-  @Column()
-  lastName: string;
+  // @OneToMany(() => Post, (post) => post.user)
+  // posts: Post[];
 
-  @Column()
-  age: number;
+  // @OneToMany(() => Vote, (vote) => vote.user)
+  // votes: Vote[];
+
+  // User 엔티티가 데이터베이스에 삽입되기 전에 해당 엔티티의 password 필드를 bcrypt를 이용하여 해싱하는 작업
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
+
+/**
+ * @OneToMany
+ * (typeFunction, inverseSideProperty)
+ * typeFunction은 매핑할 다른 엔티티의 클래스 타입을 지정하는 함
+ * inverseSideProperty는 연결된 다른 엔티티에서 현재 엔티티와 관련된 역방향 참조를 나타내는 속성 이름을 지정
+ */
