e1b8103c10fb6e3d027eb92b5c070014009ec180
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-06 20:33:08 +0900

    Feat: [server] base Entity 작성

---
+++ server/src/entities/Entity.ts
@@ -0,0 +1,22 @@
+import { instanceToPlain } from 'class-transformer';
+import {
+  BaseEntity,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export default abstract class Entity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  toJSON() {
+    return instanceToPlain(this);
+  }
+}
