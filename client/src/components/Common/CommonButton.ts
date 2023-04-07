4b2e5bfe5aa0c04b9e7691bb1c3c2151c80456a5
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-07 21:32:12 +0900

    Feat: [client] common button 컴포넌트 추가

---
+++ client/src/components/Common/CommonButton.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Button } from 'antd';
+
+export default styled(Button)`
+  position: absolute;
+  bottom: 32px;
+  left: 0;
+  width: 100%;
+  height: 44px;
+  font-size: 15px;
+  font-weight: 600;
+  border: 0;
+  background-color: ${({ theme }) => theme.primary};
+`;
