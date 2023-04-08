ecd5eb1db2a194024c1bc1060da8e25c2cb7a2e2
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-08 17:54:29 +0900

    Feat: [client] react-hot-toast 성공, 실패, 비동기 훅 작성

---
+++ client/src/hooks/useToast.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+export const successNotify = (message: string) => toast.success(message);
+export const errorNotify = (message: string) => toast(message, { icon: '😝' });
+
+export function useAsyncToast() {
+  const asyncToast = useCallback(
+    (resultPromise: any, data: any, messages: any) => {
+      toast
+        .promise(
+          resultPromise,
+          {
+            loading: messages.loading,
+            success: messages.success(data),
+            error: (err) => messages.error(err),
+          },
+          {
+            style: {
+              marginLeft: '300px',
+              minWidth: '250px',
+            },
+          }
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    []
+  );
+
+  return { asyncToast };
+}
+
+export function ToastContainer() {
+  return <Toaster position="bottom-center" reverseOrder={true} />;
+}
