624da2d6d93e8681880bdd3205e15720ea5b9218
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-17 21:10:00 +0900

    Feat: [client] 유저프로필 페이지 getServerSideProps 추가

--- client/src/pages/profile/[identifier].tsx
+++ client/src/pages/profile/[identifier].tsx
@@ -1,20 +1,23 @@
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 import { Tabs, TabsProps } from 'antd';
 import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import React from 'react';
+import { getUserInfoAPI } from '../../apis/user';
 import ProfileImage from '../../components/Common/ProfileImage';
 import ProfileCommentTab from '../../components/Common/ProfileTab/ProfileCommentTab';
 import ProfilePostTab from '../../components/Common/ProfileTab/ProfilePostTab';
 import LogoHeader from '../../components/Header/LogoHeader/LogoHeader';
 import { axiosInstance } from '../../configs/axios';
+import { useUserStore } from '../../store/userStore';
 import P from './Profile.styles';
 
-function Profile() {
+function Profile({ identifier }: { identifier: string }) {
   const items: TabsProps['items'] = [
     {
       key: '작성 글',
       label: `작성 글`,
-      children: <ProfilePostTab />,
+      children: <ProfilePostTab identifier={identifier} />,
     },
     {
       key: '작성 댓글',
@@ -28,6 +31,9 @@ function Profile() {
     },
   ];
 
+  const currentLoginUser = useUserStore((state) => state.userInfo);
+  const { data: userInfo } = getUserInfoAPI(identifier);
+
   const onChange = (key: string) => {
     console.log(key);
   };
@@ -37,18 +43,21 @@ function Profile() {
       <LogoHeader headerIcons={false} />
       <P.InfoWrapper>
         <P.InfoLeft>
-          <h2>유저이름</h2>
+          <h2>{userInfo?.username}</h2>
           <P.FollowInfoWrapper>
             <h4>팔로워 0 ·</h4>
             <h4>팔로잉 1</h4>
           </P.FollowInfoWrapper>
-          <P.EditButton>프로필 편집</P.EditButton>
+          {currentLoginUser?.username === userInfo?.username ? (
+            <P.EditButton>프로필 편집</P.EditButton>
+          ) : (
+            <P.EditButton>신고하기</P.EditButton>
+          )}
         </P.InfoLeft>
         <P.InfoRight>
-          <ProfileImage />
+          <ProfileImage src={userInfo?.image?.src} />
         </P.InfoRight>
       </P.InfoWrapper>
-
       <P.StyledTab defaultActiveKey="1" items={items} onChange={onChange} />
     </P.Wrapper>
   );
@@ -56,18 +65,33 @@ function Profile() {
 
 export default Profile;
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  try {
-    const cookie = req.headers.cookie;
-    if (!cookie) throw new Error('Missing auth token cookie');
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const queryClient = new QueryClient();
+  const queryKey = `/profile/${params?.identifier}`;
+  const queryFn = () => axiosInstance.get(queryKey).then((res) => res.data);
 
-    // 쿠키가 있으면 백엔드에서 인증 처리
-    await axiosInstance.get('/auth/me', { headers: { cookie } });
+  await queryClient.prefetchQuery([queryKey], queryFn);
 
-    return { props: {} };
-  } catch (error) {
-    // 백엔드 요청에서 준 cookie를 사용해 인증 처리할 때 에러가 나면 /login 페이지로 이동
-    res.writeHead(307, { Location: '/login' }).end();
-    return { props: {} };
-  }
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      identifier: params?.identifier,
+    },
+  };
 };
+
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   try {
+//     const cookie = req.headers.cookie;
+//     if (!cookie) throw new Error('Missing auth token cookie');
+
+//     // 쿠키가 있으면 백엔드에서 인증 처리
+//     await axiosInstance.get('/auth/me', { headers: { cookie } });
+
+//     return { props: {} };
+//   } catch (error) {
+//     // 백엔드 요청에서 준 cookie를 사용해 인증 처리할 때 에러가 나면 /login 페이지로 이동
+//     res.writeHead(307, { Location: '/login' }).end();
+//     return { props: {} };
+//   }
+// };
