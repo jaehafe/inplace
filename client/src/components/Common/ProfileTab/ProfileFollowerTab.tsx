3528a7ec6488dcae13636d7426068e4b2b6dd0bf
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-19 05:31:31 +0900

    Feat: [client] 팔로워 목록 탭 무한스크롤 api 초안

--- client/src/components/Common/ProfileTab/ProfileFollowerTab.tsx
+++ client/src/components/Common/ProfileTab/ProfileFollowerTab.tsx
@@ -1,12 +1,60 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { Button } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { axiosInstance } from '../../../configs/axios';
 import { IIdentifier } from '../../../types';
 import ProfileImage from '../ProfileImage';
 import T from './Tab.styles';
 
 function ProfileFollowerTab({ identifier }: IIdentifier) {
+  const { ref: observeRef, inView } = useInView();
+
+  const queryKey = `/follows/${identifier}/followers`;
+
+  const {
+    status,
+    data: infiniteData,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+  } = useInfiniteQuery(
+    [queryKey],
+    async ({ pageParam = 0 }) => {
+      const { data } = await axiosInstance.get(`${queryKey}?page=${pageParam}`);
+      const isLast = data.length === 0;
+
+      return {
+        result: data,
+        nextPage: pageParam + 1,
+        isLast,
+      };
+    },
+    {
+      getNextPageParam: (lastPage) => {
+        if (!lastPage.isLast) {
+          return lastPage.nextPage;
+        } else {
+          return undefined;
+        }
+      },
+      cacheTime: 60000, // 60초간 캐시된 데이터 유효
+    }
+  );
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, observeRef]);
+
   return (
-    <T.Container>
+    <T.Container ref={observeRef}>
       <T.Wrapper>
         <T.BodyLeft>
           <ProfileImage
