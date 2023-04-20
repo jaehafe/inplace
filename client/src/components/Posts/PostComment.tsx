40be3488974fe6d482cd7f3b7b42257494c91f8a
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-20 10:45:36 +0900

    Remove: [client] console.log 주석 처리

--- client/src/components/Posts/PostComment.tsx
+++ client/src/components/Posts/PostComment.tsx
@@ -124,8 +124,8 @@ function PostComment({ data }: any) {
       );
     }
   };
-  console.log('currentLoginUser>>>', currentLoginUser);
-  console.log('commentVotes>>', commentVotes);
+  // console.log('currentLoginUser>>>', currentLoginUser);
+  // console.log('commentVotes>>', commentVotes);
 
   const checkWhetherVoted = (currentLoginUser: any) => {
     const alreadyVote = commentVotes.find(
