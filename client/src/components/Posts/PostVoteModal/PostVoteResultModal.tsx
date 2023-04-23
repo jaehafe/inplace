a070067c44e2e7f530f06c5fba63f6c95a6bb082
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-23 20:22:00 +0900

    Feat: [client] 게시물 투표 분석 Drawer 로딩 처리

--- client/src/components/Posts/PostVoteModal/PostVoteResultModal.tsx
+++ client/src/components/Posts/PostVoteModal/PostVoteResultModal.tsx
@@ -6,10 +6,11 @@ import L from '../../Header/LogoHeader/LogoHeader.styles';
 import { AppImages } from '../../../configs/AppImages';
 import { getPostVoteResultAPI } from '../../../apis/post';
 import PostVoteResult from './PostVoteResult';
+import { Spin } from 'antd';
 
 function PostVoteResultModal() {
   const { isOpen, postId, closeModal } = usePostVoteResultModalStoreActions();
-  const { data: detailPost, isLoading } = getPostVoteResultAPI(postId!);
+  const { data, isLoading, isFetching } = getPostVoteResultAPI(postId!);
 
   const openModalFn = () => {
     if (isOpen && postId) {
@@ -21,29 +22,27 @@ function PostVoteResultModal() {
 
   return (
     <>
-      {!isLoading && detailPost && (
-        <P.PostVoteResultDrawer
-          placement="bottom"
-          title={
-            <L.DrawerHeader>
-              <Image
-                src={AppImages.InPlaceLogo}
-                alt="profileImage"
-                width="30"
-                height="30"
-              />
-              투표 결과 분석
-            </L.DrawerHeader>
-          }
-          closable={false}
-          onClose={closeModal}
-          open={openModalFn()}
-          key="bottom"
-          height={'auto'}
-        >
-          <PostVoteResult data={detailPost} />
-        </P.PostVoteResultDrawer>
-      )}
+      <P.PostVoteResultDrawer
+        placement="bottom"
+        title={
+          <L.DrawerHeader>
+            <Image
+              src={AppImages.InPlaceLogo}
+              alt="profileImage"
+              width="30"
+              height="30"
+            />
+            투표 결과 분석
+          </L.DrawerHeader>
+        }
+        closable={false}
+        onClose={closeModal}
+        open={openModalFn()}
+        key="bottom"
+        height={'auto'}
+      >
+        {data ? <PostVoteResult data={data} /> : <Spin />}
+      </P.PostVoteResultDrawer>
     </>
   );
 }
