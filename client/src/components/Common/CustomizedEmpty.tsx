728f9aaafc950106fd8f7b4bbcf7161a67d25124
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-23 22:47:37 +0900

    Refactor: [client] 확장성 고려, 컴포넌트 리팩토링

--- client/src/components/Common/CustomizedEmpty.tsx
+++ client/src/components/Common/CustomizedEmpty.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { Empty } from 'antd';
+import React, { CSSProperties } from 'react';
+import { Button, Empty } from 'antd';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { useUserInfo } from '../../store/userStore';
 
 interface ICustomizedEmpty {
   desc1?: string;
   desc2?: string;
+  buttonMessage?: string;
 }
 
-function CustomizedEmpty({ desc1, desc2 }: ICustomizedEmpty) {
+const EmptyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  & span:nth-of-type(2) {
+  }
+`;
+
+function CustomizedEmpty({ desc1, desc2, buttonMessage }: ICustomizedEmpty) {
+  const currentLoginUser = useUserInfo();
+
   return (
     <Empty
       image={Empty.PRESENTED_IMAGE_SIMPLE}
       description={
-        <>
-          <span style={{ display: 'block' }}>{desc1}</span>
+        <EmptyWrapper>
+          <span>{desc1}</span>
           <span>{desc2}</span>
-        </>
+
+          {buttonMessage && (
+            <Link href="/post/create">
+              <Button>{buttonMessage}</Button>
+            </Link>
+          )}
+        </EmptyWrapper>
       }
     />
   );
