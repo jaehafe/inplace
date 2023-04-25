267deb1faec04e32a5f5af96c45b82105b889768
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-25 20:52:21 +0900

    Feat: [client] 카테고리 페이지 카테고리 id getServerSideProps로 가져오기

---
+++ client/src/pages/category/[identifier].tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import LogoHeader from '../../components/Header/LogoHeader/LogoHeader';
+import P from '../profile/Profile.styles';
+import Category from '../../components/Category/Category';
+import { GetServerSideProps } from 'next';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { axiosInstance } from '../../configs/axios';
+
+function CategoryPage({ identifier }: { identifier: string }) {
+  return (
+    <P.Wrapper>
+      <LogoHeader headerIcons={true} />
+      <Category categoryId={identifier} />
+    </P.Wrapper>
+  );
+}
+
+export default CategoryPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const queryClient = new QueryClient();
+  const queryKey = `/categories/${params?.identifier}`;
+  const queryFn = () => axiosInstance.get(queryKey).then((res) => res.data);
+
+  await queryClient.prefetchQuery([queryKey], queryFn);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      identifier: params?.identifier,
+    },
+  };
+};
