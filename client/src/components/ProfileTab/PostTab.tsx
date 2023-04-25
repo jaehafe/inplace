2f0a75b4b6e3318c72cc2097ac58edf3c9a71477
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-25 20:49:56 +0900

    Feat: [client] 프로필 탭 - TagList 컴포넌트 추가

--- client/src/components/ProfileTab/PostTab.tsx
+++ client/src/components/ProfileTab/PostTab.tsx
@@ -12,6 +12,7 @@ import { Divider } from 'antd';
 
 import Link from 'next/link';
 import { formattedDate } from '../../utils';
+import TagList from '../Common/Tags/TagList';
 
 function PostTab({ post }: any) {
   const {
@@ -22,37 +23,41 @@ function PostTab({ post }: any) {
     title,
     username,
     images,
+    categories,
   } = post;
 
   return (
-    <Link href={`/post/${identifier}`}>
-      <T.BodyWrapper>
-        <ProfileImage src={images[0]?.src} style={{ marginRight: '14px' }} />
-        <T.Body>
-          <h4>{title}</h4>
-          <span>
-            by {username} · {formattedDate(updatedAt)}
-          </span>
-        </T.Body>
-      </T.BodyWrapper>
-      <T.Footer>
-        <T.VoteWrapper>
-          <LikeTwoTone twoToneColor="#2515d5" />
-          <FrownTwoTone twoToneColor="#eb2f96" />
-          <DislikeTwoTone twoToneColor="#52c41a" />
-          <span>{voteScore}</span>
-        </T.VoteWrapper>
-        <T.CommentWrapper>
-          <CommentOutlined />
-          <span>{commentCount}</span>
-        </T.CommentWrapper>
-        <T.ViewWrapper>
-          <EyeOutlined />
-          <span>123</span>
-        </T.ViewWrapper>
-      </T.Footer>
+    <>
+      <Link href={`/post/${identifier}`}>
+        <T.BodyWrapper>
+          <ProfileImage src={images[0]?.src} style={{ marginRight: '14px' }} />
+          <T.Body>
+            <h4>{title}</h4>
+            <span>
+              by {username} · {formattedDate(updatedAt)}
+            </span>
+          </T.Body>
+        </T.BodyWrapper>
+        <T.Footer>
+          <T.VoteWrapper>
+            <LikeTwoTone twoToneColor="#2515d5" />
+            <FrownTwoTone twoToneColor="#eb2f96" />
+            <DislikeTwoTone twoToneColor="#52c41a" />
+            <span>{voteScore}</span>
+          </T.VoteWrapper>
+          <T.CommentWrapper>
+            <CommentOutlined />
+            <span>{commentCount}</span>
+          </T.CommentWrapper>
+          <T.ViewWrapper>
+            <EyeOutlined />
+            <span>123</span>
+          </T.ViewWrapper>
+        </T.Footer>
+      </Link>
+      <TagList categories={categories} />
       <Divider style={{ margin: '14px 0' }} />
-    </Link>
+    </>
   );
 }
 
--- client/src/pages/tags.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Tags() {
-  return <div>tags</div>;
-}
-
-export default Tags;
