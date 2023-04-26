c8fbe7374576c2ea7590779516ce0363c388ee64
Jaeha Lee <wogkdkrm112@gmail.com>
2023-04-26 22:34:42 +0900

    Conf: [server] data-source host 이름 변경

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'inplace',
+  host: 'localhost',
   port: 3306,
   username: 'root',
   password: '123456789',
